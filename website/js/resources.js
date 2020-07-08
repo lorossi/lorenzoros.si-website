44c1908a6d339e173462e9e2f840dfda1d55c03f
lorossi <lo.rossi@live.com>
2020-07-08 20:36:29 +0200

    added resources.js file

---
+++ website/js/resources.js
@@ -0,0 +1,38 @@
+var colors = [
+    ['#1ABC9C', '#2ECC71', true],
+    ['#1ABC9C', '#3498DB', true],
+    ['#1ABC9C', '#27AE60', true],
+    ['#1ABC9C', '#2980B9', true],
+    ['#2ECC71', '#3498DB', true],
+    ['#2ECC71', '#16A085', true],
+    ['#2ECC71', '#2980B9', true],
+    ['#3498DB', '#9B59B6', false],
+    ['#3498DB', '#16A085', true],
+    ['#3498DB', '#27AE60', true],
+    ['#3498DB', '#8E44AD', false],
+    ['#9B59B6', '#2980B9', false],
+    ['#16A085', '#27AE60', true],
+    ['#16A085', '#2980B9', false],
+    ['#27AE60', '#2980B9', false],
+    ['#2980B9', '#8E44AD', false],
+    ['#F1C40F', '#E74C3C', true],
+    ['#F1C40F', '#D35400', true],
+    ['#F1C40F', '#C0392B', true],
+    ['#E67E22', '#E74C3C', false],
+    ['#E67E22', '#C0392B', false],
+    ['#E74C3C', '#F39C12', true],
+    ['#F39C12', '#C0392B', false]
+];
+
+var subtitles = [
+  "not a designer",
+  "not a full stack programmer",
+  "not (yet) an engineer",
+  "a good programmer",
+  "a creative guy",
+  "many good ideas",
+  "eager to learn",
+  "can use stackoverflow",
+  "visit my GitHub",
+  "likes minimalistic design"
+];
