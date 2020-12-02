526ecc0885c096d0435b267d06eac5d419fb5890
lorossi <lo.rossi@live.com>
2020-12-02 16:20:43 +0100

    cleaned up code

--- public_html/js/animations.js
+++ public_html/js/animations.js
@@ -1,39 +1,22 @@
 /*jshint esversion: 6 */
-
+let selectors = [".sketch", ".navbar", "#page1", "#page2", "#page3"];
 
 function animate() {
   let duration = 1000;
   let bottom = $(document).scrollTop() + $(window).height();
 
-  if (bottom <= $("#page1").outerHeight() * 1.5 && $("#page1").css("display") == "none") {
-    setTimeout(() => {
-      $("#page1").fadeIn(duration);
-    }, duration / 2);
-    setTimeout(() => {
-      $(".navbar").fadeIn(duration);
-    }, duration);
-    setTimeout(() => {
-      $(".sketch").fadeIn(duration);
-    }, duration);
-
-  }
-
-  if (bottom > $("#page2").offset().top && $("#page2").css("display") == "none" && $("#logo").css("display") == "block") {
-    setTimeout(() => {
-      $("#page2").fadeIn(duration);
-    }, duration / 2);
-  }
-
-  if (bottom > $("#page3").offset().top && $("#page3").css("display") == "none" && $("#page2").css("display") == "block") {
-    setTimeout(() => {
-      $("#page3").fadeIn(duration);
-    }, duration / 2);
-  }
-
+  selectors.forEach((s, i) => {
+    if (bottom >= $(s).offset().top && $(s).css("display") == "none") {
+      $(s).fadeIn(duration);
+    }
+  });
 }
 
 $(document).ready(() => {
-  $(".navbar, #page1, #page2, #page3").css("display", "none");
+  selectors.forEach((s, i) => {
+    $(s).css("display", "none");
+  });
+
   animate();
 });
 
