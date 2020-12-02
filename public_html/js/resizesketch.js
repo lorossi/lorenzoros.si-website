c2f12d2dbf87f0fa4890edc15e846392f7c78cef
lorossi <lo.rossi@live.com>
2020-12-02 17:49:08 +0100

    fixed a missing var

--- public_html/js/resizesketch.js
+++ public_html/js/resizesketch.js
@@ -1,6 +1,7 @@
 // this script moves pages according to their final position
 /*jshint esversion: 6 */
 
+let page_width = 0;
 
 function placeElements() {
   sketch_selector = "#sketch";
@@ -8,7 +9,7 @@ function placeElements() {
 
   let canvas_height = $("#page1").height() + $("#page2").height() + $("#page3").height();
   let canvas_width = $(window).width();
-  let fade_height = canvas_height / 5;
+  let fade_height = canvas_height / 4;
 
   $(sketch_selector).prop("width", canvas_width);
   $(sketch_selector).prop("height", canvas_height);
