6b463be65d9f2f58e87c5854bfbd3c1e9bef143f
lorossi <lo.rossi@live.com>
2020-12-06 15:40:02 +0100

    updated show portfolio
     Languages without repos are not shown anymore

--- public_html/js/showportfolio.js
+++ public_html/js/showportfolio.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 8 */
+
 // this script takes care of showing my projects
 // one day i'll update my hosting to a VPS so all of this won't be necessary at all - flask ftw
 // made by Lorenzo Rossi - www.lorenzoros.si
@@ -7,7 +9,7 @@ $(document).ready(function() {
   let stats_container = "#portfolio .codestats";
 
   let languages = new Set(resources.repos.map((r) => {
-    return r.main_language
+    return r.main_language;
   }));
   languages = Array.from(languages);
 
@@ -20,20 +22,26 @@ $(document).ready(function() {
       let selected_repos = [];
       resources.repos.forEach((r, j) => {
         if (r.selected && r.main_language === l) {
-          let string = `<a href="${r.url}">${r.formatted_name}</a>`
+          let string = `<a href="${r.url}">${r.formatted_name}</a>`;
           selected_repos.push(string);
         }
-      })
+      });
+
       new_element += `${selected_repos.join(", ")}`;
       new_element += `</td>`;
 
-      $(repo_container).append(new_element);
-  })
+      console.log(l, selected_repos);
+
+      // skips languages that don't have any repo
+      if (selected_repos.length > 0) {
+          $(repo_container).append(new_element);
+      }
+  });
 
   $(stats_container + " #repos").text(resources.stats.total_repos);
   $(stats_container + " #commits").text(resources.stats.total_commits);
   $(stats_container + " #languages").text(resources.stats.total_languages);
   $(stats_container + " #stars").text(resources.stats.total_stars);
 
-  placeElements(); // ...
+  placeElements();
 });
