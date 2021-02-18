6ff365d14dac69a1d8e5fd1b26e0e1569158fc83
lorossi <mail@lorenzoros.si>
2021-02-18 22:18:27 +0100

    fixes and improvements

--- public_html/js/language.js
+++ public_html/js/language.js
@@ -3,26 +3,20 @@
 
 /*jshint esversion: 6 */
 
+let active_language;
 $(document).ready(function() {
-  let list_selector = "li"; // elements that must be displayed as list
-  let cell_selector = "td"; // elements that must be displayed as table cell
   let language = navigator.language || navigator.userLanguage;
 
   if (language === "it-IT") {
-    $("*:lang(it)").css({"display": "inline-block"});
-    $("*:lang(it) " + list_selector).css({"display": "list-item"});
-    $("*:lang(en)").css({"display": "none"});
+    $("*:lang(it-IT)").css({"display": "inline-block"});
+    $("*:lang(en-EN)").css({"display": "none"});
     active_language = "it-IT";
   } else {
     active_language = "en-EN";
   }
 
   $(".languageswitch").click(function() {
-    let short_language, selector;
-
-    short_language = active_language.slice(0, 2); // en for english and it for italian
-    selector = `*:lang(${short_language})`;
-    $(selector).css({"display": "none"});
+    $(`*:lang(${active_language})`).css({"display": "none"});
 
     if (active_language === "it-IT") { // switch languages around
       active_language = "en-EN";
@@ -30,12 +24,7 @@ $(document).ready(function() {
       active_language = "it-IT";
     }
 
-    short_language = active_language.slice(0, 2); // en for english and it for italian
-    selector = `*:lang(${short_language})`;
-    $(selector).css({"display": "inline-block"});
-
-    $(selector + " " + list_selector).css({"display": "list-item"});
-    $(selector + " " + cell_selector).css({"display": "table-cell"});
+    $(`*:lang(${active_language})`).css({"display": "inline-block"});
   });
 
 });
