144c192822df69f7b9b890d01f8de1d990ae1fe6
lorossi <mail@lorenzoros.si>
2023-01-11 16:58:12 +0100

    updated the code for the typer, cleaned a lot!

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -17,7 +17,7 @@ const add_lines = () => {
     const line = document.createElement("div");
     line.classList.add("line");
     // random position
-    line.style.top = `${Math.random() * 100}%`;
+    line.style.top = `${Math.random() * max_height}px`;
     line.style.animationDuration = `${Math.random() * 0.3 + 0.2}s`;
     line.style.animationDelay = `${Math.random() * 2}s`;
     // add to container
@@ -25,60 +25,43 @@ const add_lines = () => {
   }
 };
 
-const print_letters = async (
-  selector = ".typer p",
-  speed = 25,
-  newline_pause = 500
-) => {
-  const dest_items = [...document.querySelectorAll(selector)];
-  const to_write = dest_items.map((e) => e.getAttribute("data-text").split(""));
-
-  await timeout(500);
-
-  return new Promise((resolve) => {
-    let item_count = 0;
-    let letter_count = 0;
-
-    const type = async () => {
-      if (letter_count == 0) {
-        // add the blinking cursor
-        dest_items[item_count].classList.add("writing", "newline");
-      }
-
-      // next letter to be written
-      const current_letter = to_write[item_count][letter_count];
-      // append letter to container
-      dest_items[item_count].append(current_letter);
-      // increase letter count to proceed
-      letter_count++;
-
-      // finished a a container item
-      if (letter_count >= to_write[item_count].length) {
-        // wait a little bit
-        await timeout(newline_pause);
-        // remove the blinking cursor if this isn't the last line
-        if (item_count < dest_items.length - 1) {
-          dest_items[item_count].classList.remove("writing");
-        }
-
-        letter_count = 0;
-        item_count++;
-      }
-
-      if (item_count < dest_items.length) {
-        // if the full write isn't ended yet, call this function again
-        await timeout(speed);
-        type();
-      } else if (item_count == dest_items.length) {
-        return resolve("done");
-      }
-    };
-
-    // bootstrap the function
-    type();
-  });
+const add_paragraph = (container) => {
+  const p = document.createElement("p");
+  p.classList.add("writing");
+  container.appendChild(p);
+  return p;
 };
 
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const remove_cursor = (p) => p.classList.remove("writing");
+
+const print_letters = async () => {
+  const selector = ".typer";
+  const typing_pause = 30;
+  const newline_pause = 500;
+  const start_pause = 250;
+
+  const container = document.querySelector(selector);
+
+  let to_write = container
+    .getAttribute("data-text")
+    .replaceAll("<br>", "\n")
+    .split("");
+  let current_p = add_paragraph(container);
+
+  await timeout(start_pause);
+
+  while (to_write.length > 0) {
+    const current_char = to_write.shift();
+
+    if (current_char == "\n") {
+      remove_cursor(current_p);
+      current_p = add_paragraph(container);
+      await timeout(newline_pause);
+    } else {
+      current_p.innerHTML += current_char;
+      await timeout(typing_pause);
+    }
+  }
+};
+
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
