bfab064501df79fab24bb225a7d4d840a1657233
Lorenzo Rossi <mail@lorenzoros.si>
2026-01-12 14:27:45 +0100

    more printing

--- python_scripts/ci/environment.py
+++ python_scripts/ci/environment.py
@@ -22,9 +22,12 @@ def setup(
                 print(f"Loading {env_var} from environment.")
                 settings[section][key] = env_value
 
+    print("Setting key filename in deployer settings.")
     settings["Deployer"]["key_filename"] = key_filename
+    env_key_var = "DEPLOYER_KEY_CONTENT"
+    print(f"Writing deployer from environment variable {env_key_var}.")
     with open(key_filename, "w") as f:
-        key_content = os.environ.get("DEPLOYER_KEY_CONTENT", "")
+        key_content = os.environ.get(env_key_var, "")
         f.write(key_content)
 
     with open(settings_file, "w") as f:
